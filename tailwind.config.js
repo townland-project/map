425e983cf6540067a33e5d826e5f8e62e643ebc6
Artemis <artemis@townland.xyz>
2022-01-20 13:43:34 +0330

    some icons added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
       backgroundImage: {
         'app-logo': "url('../public/assets/logo.png')",
         'location': "url('../public/assets/location.png')",
+        'home': "url('../public/assets/home.png')",
+        'menu': "url('../public/assets/menu.png')",
+        'search': "url('../public/assets/search.png')",
       }
     },
   },
